4af8782be27924314b090bc6c236861bf1a49e57
1007424128@qq.com <answer369>
2020-03-09 21:57:54 +0800

    "线性代数奇异值分解"1

---
+++ pystudy/probability_statistics/math_gitchat/linear_algebra/linear_equations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@File : linear_equations.py
+@Author : jeffsheng
+@Date : 2020/3/1 0001
+@Desc : python求解线性方程组
+"""
+
+import numpy as np
+from scipy import linalg
+
+A = np.array([[1, 2, 3],
+              [1, -1, 4],
+              [2, 3, -1]])
+
+y = np.array([14, 11, 5])
+
+x = linalg.solve(A, y)
+# [1. 2. 3.]
+print(x)
+
+
---
+++ pystudy/probability_statistics/math_gitchat/linear_algebra/singular_value_decomposition.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@File : singular_value_decomposition.py
+@Author : jeffsheng
+@Date : 2020/3/9 0009
+@Desc : 通过 Python 提供的工具，直接一次性获得奇异值分解的所有结果
+"""
+import numpy as np
+
+A=[[0, 0, 0, 2, 2],
+   [0, 0, 0, 3, 3],
+   [0, 0, 0, 1, 1],
+   [1, 1, 1, 0, 0],
+   [2, 2, 2, 0, 0],
+   [5, 5, 5, 0, 0],
+   [1, 1, 1, 0, 0]]
+
+U, sigma, VT = np.linalg.svd(A)
+
+print(U)
+print("-------------")
+# sigma是奇异值按照从大到小顺序组成的列表
+print(sigma)
+print("-----打印V的转置--------")
+print(VT)
+
+print("-----------------------")
+# 利用奇异值分解的结果进行行压缩和列压缩
+A=[[0, 0, 0, 2, 2],
+   [0, 0, 0, 3, 3],
+   [0, 0, 0, 1, 1],
+   [1, 1, 1, 0, 0],
+   [2, 2, 2, 0, 0],
+   [5, 5, 5, 0, 0],
+   [1, 1, 1, 0, 0]]
+
+U, sigma, VT = np.linalg.svd(A)
+
+U_T_2x7 = U.T[:2,:]
+print(np.dot(U_T_2x7,A))
+
+VT_2x5=VT[:2,:]
+print(np.dot(VT_2x5,np.mat(A).T).T)
+
+
+print("-----------------------------")
+# 利用数据压缩进行矩阵近似
+A=[[0, 0, 0, 2, 2],
+   [0, 0, 0, 3, 3],
+   [0, 0, 0, 1, 1],
+   [1, 1, 1, 0, 0],
+   [2, 2, 2, 0, 0],
+   [5, 5, 5, 0, 0],
+   [1, 1, 1, 0, 0]]
+
+U, sigma, VT = np.linalg.svd(A)
+A_1 = sigma[0]*np.dot(np.mat(U[:, 0]).T, np.mat(VT[0, :]))
+A_2 = sigma[1]*np.dot(np.mat(U[:, 1]).T, np.mat(VT[1, :]))
+print(A_1+A_2)
